c06e1ff28efb64cdf4990e4528de50e5af0a29a7
22mahmoud <mahmouds1997@yahoo.com>
2018-03-03 23:03:51 +0200

    define WEBGL_RENDERER and CANVAS_RENDERER as a global vars'

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import * as Phaser from 'phaser'
-
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload,
-        create,
-        update
-    }
-}
-
-const game = new Phaser.Game(config);
+import Phaser from 'phaser';
 
 function preload() {}
 
 function create() {}
 
 function update() {}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload,
+    create,
+    update,
+  },
+};
+
+// eslint-disable-next-line no-unused-vars
+const game = new Phaser.Game(config);

dfaab01b4f94b4ada8da0892bc02ab527aa9cc17
22mahmoud <mahmouds1997@yahoo.com>
2018-03-03 23:28:40 +0200

    load assets

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import Phaser from 'phaser';
 
-function preload() {}
+function preload() {
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('ground', 'assets/platform.png');
+  this.load.image('star', 'assets/star.png');
+  this.load.image('bomb', 'assets/bomb.png');
+  this.load.spritesheet('dude', 'assets/dude.png', {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
+let platforms;
+function create() {
+  this.add.image(400, 300, 'sky');
 
-function create() {}
+  platforms = this.physics.add.staticGroup();
+
+  platforms
+    .create(400, 568, 'ground')
+    .setScale(2)
+    .refreshBody();
+
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+}
 
 function update() {}
 
 const config = {
-  type: Phaser.AUTO,
+  type: Phaser.CANVAS,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
   scene: {
     preload,
     create,

1d21f216c82575b1f2da81a86cb838f6ff1b62ec
22mahmoud <mahmouds1997@yahoo.com>
2018-03-09 22:44:13 +0200

    copy assets folder into dist

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import Phaser from 'phaser';
 
 function preload() {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('ground', 'assets/platform.png');
-  this.load.image('star', 'assets/star.png');
-  this.load.image('bomb', 'assets/bomb.png');
-  this.load.spritesheet('dude', 'assets/dude.png', {
+  this.load.image('sky', 'dist/assets/sky.png');
+  this.load.image('ground', 'dist/assets/platform.png');
+  this.load.image('star', 'dist/assets/star.png');
+  this.load.image('bomb', 'dist/assets/bomb.png');
+  this.load.spritesheet('dude', 'dist/assets/dude.png', {
     frameWidth: 32,
     frameHeight: 48,
   });
 }
 let platforms;
+
 function create() {
   this.add.image(400, 300, 'sky');
 
@@ -35,7 +36,9 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 300 },
+      gravity: {
+        y: 300,
+      },
       debug: false,
     },
   },

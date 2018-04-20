b85326e7bc5cdbb0242b539464186e2837c130fb
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2018-04-20 11:34:10 -0300

    Fixed ESLint configuration, added related package scripts.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
+  env: {browser: true},
   extends: 'airbnb-base',
   rules: {
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': ['error', 'unix'],
   },
-  plugins: ['import'],
+  plugins: ['import']
 };
